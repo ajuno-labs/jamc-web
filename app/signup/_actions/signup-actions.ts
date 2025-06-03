2231b8e9233eb7a7b99ded46b7395878022cd973
Giao Le <giolynx104@gmail.com>
2025-06-03 09:37:15 +0700

    fix: password hashing problem in sign up flow

--- app/signup/_actions/signup-actions.ts
+++ app/signup/_actions/signup-actions.ts
@@ -2,7 +2,6 @@
 
 import { getPublicEnhancedPrisma } from "@/lib/db/enhanced"
 import { signUpSchema, type SignUpInput } from "@/lib/types/auth"
-import bcrypt from "bcryptjs"
 import { z } from "zod"
 
 export async function signUpUser(data: SignUpInput) {
@@ -24,15 +23,12 @@ export async function signUpUser(data: SignUpInput) {
       }
     }
     
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(validatedData.password, 12)
-    
-    // Create the user
+    // Create the user - ZenStack will automatically hash the password
     const user = await prisma.user.create({
       data: {
         name: validatedData.name,
         email: validatedData.email,
-        password: hashedPassword
+        password: validatedData.password
       }
     })
     
